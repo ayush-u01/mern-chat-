88e95c4c201f3c3e5cbd8b41b1fbd7db8aae0ddd
AYUSH UPADHYAY <77583924+ayush-u01@users.noreply.github.com>
2022-10-02 19:58:05 +0530

    .

---
+++ back/data.js
@@ -0,0 +1,35 @@
+const chats = [
+    {
+        isgpchat : false,
+        users: [
+            {
+                name : " john doe",
+                email : "john@eg.com",
+            },
+            {
+                name : " piyusg doe",
+                email : "piy@eg.com",
+            }
+        ],
+        _id : "743348",
+        chatname : "john doe",
+    },
+    {
+        isgpchat : false,
+        users: [
+            {
+                name : " rahul doe",
+                email : "ra@eg.com",
+            },
+            {
+                name : " dhuv doe",
+                email : "dhv@eg.com",
+            }
+        ],
+        _id : "8439843",
+        chatname : "rahul doe",
+    },
+];
+
+
+module.exports = {chats};
--- back/server.js
+++ back/server.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require('./data');
 const app = express();
 
-app.listen(3000, console.log('server started on port 3000'));
+dotenv.config();
+
+app.get('/', (req, res)=> {
+    res.send("API is running");
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singlechat = chats.find(c=> c._id == req.params.id);
+    res.send(singlechat);
+    // res.send
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`)); 
