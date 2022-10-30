9cb8f4edcf8245162191db854537bca45f2d92fc
AYUSH UPADHYAY <77583924+ayush-u01@users.noreply.github.com>
2022-10-30 12:38:47 +0530

    chat page routes completed

--- back/routes/chatRoutes.js
+++ back/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { accessChat, fetchChats, createGroupChat } = require('../controller/chatController');
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup } = require('../controller/chatController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router =  express.Router();
@@ -7,10 +7,10 @@ const router =  express.Router();
 router.route('/').post(protect, accessChat) // used protect so only logged in user can access this chat, not others
 router.route('/').get(protect, fetchChats)
 router.route('/group').post(protect, createGroupChat);
+router.route('/rename').put(protect, renameGroup);
 // router.route('/rename').put(protect, renameGroup);
-// router.route('/rename').put(protect, renameGroup);
-// router.route('/groupremove').put(protect, removeFormGroup);
-// router.route('/groupadd').put(protect, addToGroup);
+router.route('/groupremove').put(protect, removeFromGroup);
+router.route('/groupadd').put(protect, addToGroup);
 
 
 module.exports = router;
